226485f259f5493849b27445a1e3f55a2890d587
Rioriea <144937104+Rioriea@users.noreply.github.com>
2023-11-23 19:20:56 -0500

    Add files via upload
    menu scripts

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+   public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetRounds(float rounds)
+    {
+        Debug.Log(rounds);
+    }
+
+    public void SetPoints(float points)
+    {
+        Debug.Log(points);
+    }
+
+
+}
